ab29eeb5cec7b35062ef50da8fe0a347880a5cd7
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-01-08 03:23:23 +0200

    clarified uuid absence error

--- main.ts
+++ main.ts
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   const uuid = req.header('uuid');
   if(!uuid) {
-    res.status(404).json(JSON.stringify('Не знайдено'));
+    res.status(404).json(JSON.stringify('Ідентифікаційний код не знайдено'));
     return;
   }
 
