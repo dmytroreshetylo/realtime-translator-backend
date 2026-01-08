b549f9c0d41a219b4ed95730f14e1a4e36b3f66e
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-01-08 02:17:06 +0200

    Add CORS support and fix route paths in main application

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { historyRouter } from './routes/history/history.route';
 import { translateRouter } from './routes/translate/translate.route';
 
@@ -7,10 +8,12 @@ export const app = express();
 
 const port = 3000;
 
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(express.json());
 app.use(bodyParser.json());
 
-app.use('history', historyRouter);
-app.use('translate', translateRouter);
+app.use('/history', historyRouter);
+app.use('/translate', translateRouter);
 
 export const server = app.listen(port, () => {
   console.log(`Сервер запустився за порту ${port}`);
