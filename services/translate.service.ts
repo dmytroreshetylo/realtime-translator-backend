9e578f302035cfe40c7af0bc6d1a662cc2af9619
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-01-08 04:20:20 +0200

    Replace Reverso API with Google Translate API in TranslateService

--- services/translate.service.ts
+++ services/translate.service.ts
@@ -1,17 +1,15 @@
-const Reverso = require('reverso-api');
+import { translate } from '@vitalets/google-translate-api';
 
 export class TranslateService {
-  private readonly reverso = new Reverso();
-
   async translate(text: string, originalLanguage: string, translateLanguage: string): Promise<string | undefined> {
-    try {
-      const result = await this.reverso.getTranslation(
-        text,
-        originalLanguage,
-        translateLanguage,
-      );
+    console.log(1);
 
-      return result.translations[0];
+    try {
+      const result = await translate(text, { from: originalLanguage, to: translateLanguage });
+
+      console.log(result);
+
+      return result.text;
     }
     catch (err) {
       throw new Error((err as Error).message);
