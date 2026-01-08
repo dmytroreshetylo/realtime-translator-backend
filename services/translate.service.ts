5b03596da38c1b36ed741b7487733c63a09f4fac
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-01-08 04:21:24 +0200

    remove Reverso API dependency and clean up TranslateService logging

--- services/translate.service.ts
+++ services/translate.service.ts
@@ -2,12 +2,9 @@ import { translate } from '@vitalets/google-translate-api';
 
 export class TranslateService {
   async translate(text: string, originalLanguage: string, translateLanguage: string): Promise<string | undefined> {
-    console.log(1);
 
     try {
       const result = await translate(text, { from: originalLanguage, to: translateLanguage });
-
-      console.log(result);
 
       return result.text;
     }
