1138f197286e2b80cb85b25a1a64a9aa944504e7
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-01-09 02:07:41 +0200

    updated config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-console.log(process.env['origin']);
-
 export const config = {
   dbFile: 'database.sqlite',
-  origin: process.env['origin'] || 'http://localhost:5173'
+  origin: process.env['origin'] || '*'
 };
